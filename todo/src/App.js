9edae3852c5f08f58e9c42fa4c85f3d0735c1355
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-08-10 18:30:51 -0400

    reset

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,102 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.css';
 
-import TodoList from './components/TodoList';
-import TodoForm from './components/TodoForm';
-
-import "./components/Todo.css";
-
-
-const tasks = [
-  {
-    name: 'Wake up',
-    id: 1,
-    completed: false
-  },
-  {
-    name: 'Eat',
-    id: 2,
-    completed: false
-  },
-  {
-    name: 'Code',
-    id: 3,
-    completed: false
-  },
-  {
-    name: 'Sleep',
-    id: 4,
-    completed: false
-  }
-]
-
-class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  constructor() {
-    super();
-    this.state = {
-      tasks: tasks
-    };
-  }
-
-  toggleItem = id => {
-    this.setState({
-      tasks: this.state.tasks.map(item => {
-        if (item.id === id) {
-          return {
-            ...item,
-            completed: !item.completed
-          };
-        } else {
-          return item;
-        }
-      })
-    })
-  };
-
-
-  addTask = itemName => {
-    const newTask = {
-      name: itemName,
-      id: new Date(),
-      completed: false
-    };
-    this.setState({
-      tasks: [...this.state.tasks, newTask]
-    });
-  }
-
-  handleRemove = (e) => {
-    e.preventDefault()
-    console.log(this.state.tasks)
-    debugger
-    this.setState({
-      tasks: this.state.tasks.filter((item) => item.completed === false)
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <div>
-          <h2>Welcome to your Todo App!</h2>
-          <TodoForm
-            addTask={this.addTask}
-            handleRemove={this.handleRemove}
-          />
-        </div>
-
-        <div>
-          <TodoList
-            toggleItem={this.toggleItem}
-            tasks={this.state.tasks}
-          />
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
